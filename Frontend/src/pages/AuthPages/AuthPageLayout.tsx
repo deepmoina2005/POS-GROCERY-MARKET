52ebf5a792ef526b264164cb9bc91dbb193e97ae
deepmoina2005 <deepmoinaboruah7@gmail.com>
2025-04-07 19:14:51 +0530

    update

--- Frontend/src/pages/AuthPages/AuthPageLayout.tsx
+++ Frontend/src/pages/AuthPages/AuthPageLayout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import GridShape from "../../components/common/GridShape";
-import { Link } from "react-router";
 import ThemeTogglerTwo from "../../components/common/ThemeTogglerTwo";
 
 export default function AuthLayout({
@@ -12,22 +11,18 @@ export default function AuthLayout({
     <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
       <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
         {children}
-        <div className="items-center hidden w-full h-full lg:w-1/2 bg-brand-950 dark:bg-white/5 lg:grid">
+        <div className="items-center hidden w-full h-full lg:w-1/2 bg-brand-50 dark:bg-white/5 lg:grid">
           <div className="relative flex items-center justify-center z-1">
             {/* <!-- ===== Common Grid Shape Start ===== --> */}
             <GridShape />
             <div className="flex flex-col items-center max-w-xs">
-              <Link to="/" className="block mb-4">
+              <div className="mt-6 border border-xl p-1 rounded-xl bg-brand-200">
                 <img
-                  width={231}
-                  height={48}
-                  src="/images/logo/auth-logo.svg"
+                className="rounded-xl w-[300vh]"
+                  src="/images/logo/auth.webp"
                   alt="Logo"
                 />
-              </Link>
-              <p className="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
-              </p>
+              </div>
             </div>
           </div>
         </div>
